00da60228775b4b509ded1a59ed8e4d7be29decf
ppalant <ppalant@redhat.com>
2019-04-01 16:37:12 +0200

    Responds to ATR request and receives APDU from vpcd

--- connection.c
+++ connection.c
@@ -11,71 +11,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-ssize_t sendall(SOCKET sock, const void *buffer, size_t size)
-{
-    size_t sent;
-    ssize_t r;
-
-    /* FIXME we should actually check the length instead of simply casting from
-     * size_t to ssize_t (or int), which have both the same width! */
-	for (sent = 0; sent < size; sent += r) {
-        r = send(sock, (void *) (((unsigned char *) buffer)+sent),
-                (size-sent), MSG_NOSIGNAL);
-
-		if (r < 0)
-			return r;
-	}
-
-    return (ssize_t) sent;
-}
-
-ssize_t recvall(SOCKET sock, void *buffer, size_t size) {
-    return recv(sock, buffer,
-            size, MSG_WAITALL|MSG_NOSIGNAL);
-}
-
-SOCKET opensock(uint16_t port)
-{
-    SOCKET sock;
-    socklen_t yes = 1;
-    struct sockaddr_in server_sockaddr;
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET)
-        return INVALID_SOCKET;
-
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void *) &yes, sizeof yes) != 0) 
-        goto err;
-
-#if HAVE_DECL_SO_NOSIGPIPE
-    if (setsockopt(sock, SOL_SOCKET, SO_NOSIGPIPE, (void *) &yes, sizeof yes) != 0)
-        goto err;
-#endif
-
-    memset(&server_sockaddr, 0, sizeof server_sockaddr);
-    server_sockaddr.sin_family      = PF_INET;
-    server_sockaddr.sin_port        = htons(port);
-    server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(sock, (struct sockaddr *) &server_sockaddr,
-                sizeof server_sockaddr) != 0)  {
-        perror(NULL);
-        goto err;
-    }
-
-    if (listen(sock, 0) != 0) {
-        perror(NULL);
-        goto err;
-    }
-
-    return sock;
-
-err:
-    close(sock);
-
-    return INVALID_SOCKET;
-}
-
 SOCKET connectsock(const char *hostname, uint16_t port)
 {
 	struct addrinfo hints, *res = NULL, *cur;
@@ -110,4 +45,5 @@ err:
 	freeaddrinfo(res);
 	return sock;
 }
+
 /* vim: set ts=4 sw=4 tw=0 noet expandtab: */
--- connection.h
+++ connection.h
@@ -4,18 +4,20 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-
+#include <glib.h>
 #define SOCKET int
 #define closesocket(x) close(x)
 #define VPCDPORT 35963
 #define INVALID_SOCKET -1
+#define VPCD_CTRL_LEN 	1
+#define VPCD_CTRL_OFF   0
+#define VPCD_CTRL_ON    1
+#define VPCD_CTRL_RESET 2
+#define VPCD_CTRL_ATR	4
+#define APDUBufSize 270
+#define MAX_ATR_LEN 40
 
-SOCKET opensock(uint16_t port);
 SOCKET connectsock(const char *hostname, uint16_t port);
 
-ssize_t sendall(SOCKET sock, const void *buffer, size_t size);
-ssize_t recvall(SOCKET sock, void *buffer, size_t size);
-
 #endif
-
 /* vim: set ts=4 sw=4 tw=0 noet expandtab: */
